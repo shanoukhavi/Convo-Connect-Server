41a26e39e7fb2fbe1f1461bbb1145a3e3fce6f24
AvinashMonteiro2003 <4nm21cs040@nmamit.in>
2024-04-27 18:57:21 +0530

    it is committed config file

--- constants/config.js
+++ constants/config.js
@@ -2,12 +2,14 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+    process.env.CLIENT_URL, // Your client-side URL
+    "https://convo-connect-client-seven.vercel.app" // Your client-side URL
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  credentials: true, // Allowing cookies and authorization headers
 };
 
 const CHATTU_TOKEN = "chattu-token";
 
-export { corsOptions, CHATTU_TOKEN };
+module.exports = { corsOptions, CHATTU_TOKEN };
+
