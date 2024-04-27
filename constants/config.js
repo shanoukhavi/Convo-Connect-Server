9e0358bbb444856531cd8a94fdfe3b0bb6104f37
AvinashMonteiro2003 <4nm21cs040@nmamit.in>
2024-04-27 19:06:41 +0530

    updated code

--- constants/config.js
+++ constants/config.js
@@ -3,7 +3,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:4173",
     process.env.CLIENT_URL, // Your client-side URL
-    "https://convo-connect-client-seven.vercel.app" // Your client-side URL
+    
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // Allowing cookies and authorization headers
